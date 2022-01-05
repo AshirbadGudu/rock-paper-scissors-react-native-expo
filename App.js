91c72a590e1b0211ac8affc94764fd7e7424b51b
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-01-05 21:26:15 +0530

    Game Screen Show Hide Logic Implemented

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Counter, Welcome } from "./screens";
 
 export default function App() {
+  const [currentScreen, setCurrentScreen] = useState("Welcome");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {currentScreen === "Welcome" ? (
+        <Welcome setCurrentScreen={setCurrentScreen} />
+      ) : (
+        <Counter />
+      )}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
